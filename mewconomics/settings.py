126a819ffefb338051fdc2bb291a339824355ffb
Han <binhhan7@gmail.com>
2024-11-26 16:52:01 +0800

    recollected static

--- mewconomics/settings.py
+++ mewconomics/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'main/staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
