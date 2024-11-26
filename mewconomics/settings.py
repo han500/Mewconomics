57488283e4744e0f767c1400f3bf7d2df523f613
Han <binhhan7@gmail.com>
2024-11-26 17:37:05 +0800

    cleared database

--- mewconomics/settings.py
+++ mewconomics/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mewconomics.wsgi.application'
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db1.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
